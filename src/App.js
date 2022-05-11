4e9c9f6273c2bd9174209506478c2f15dc12c2a1
Sandeep Magar <sandi.magar@gmail.com>
2022-05-11 13:38:13 -0700

    auto update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with SandiBoy
+          Learn React with SandiBoy & Pradnya
         </a>
       </header>
     </div>
