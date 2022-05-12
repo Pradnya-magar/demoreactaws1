5d5df552150993ae9904c1b9aa93effda8438792
Pradnya <pradnya.magar@outlook.com>
2022-05-12 11:53:56 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with SandiBoy & Pradnya & Neil
+          Learn React with SandiBoy & Pradnya & Neil Magar
         </a>
       </header>
     </div>
